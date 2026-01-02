7589778633da44d98977894e2d30c7b64a6c69f0
JoaoBerton <141670692+JoaoBerton@users.noreply.github.com>
2026-01-02 13:43:10 -0300

    ex008.js e ex009.js criados.

---
+++ aula06/aula11/ex009.js
@@ -0,0 +1,7 @@
+let not = 75.8
+console.log(`A nota do aluno durante o trimestre foi: ${not}`)
+if(not => 65){
+    console.log(`Muito bem de acordo com a sua nota durante o trimesntre ${not}, você esta APROVADO!`)
+} else{
+    console.log(`De acordo com sua nota ${not}, Você foi REPROVADO! `)
+}
