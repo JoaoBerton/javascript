aa83e79132632239b14d6eb0b37505ee5830277d
JoaoBerton <141670692+JoaoBerton@users.noreply.github.com>
2026-03-03 14:04:06 -0300

    Criação de arquivos novos, testando for e while.

---
+++ aula12/ex015/ex016.js
@@ -0,0 +1,15 @@
+//Testando o uso de estruturas de repetição (While, do while).
+//Esta é a estrutura de repetição "While"!
+let num = 11
+while(num <= 30) {
+    console.log(`Teste ${num}`)
+    num++
+}
+/*
+//Esta é a estrutura de rep "Do While"!
+let n = 50
+do{
+    console.log(`Teste N° ${n} sendo exibido!`)
+    n--
+}while(n >= 10)
+*/
---
+++ aula12/ex015/ex017.js
@@ -0,0 +1,16 @@
+//Testando o comando For "Estruturas de repetição".
+
+//Comparação com o While...
+/*
+let n = 1
+while(n > 30){
+    console.log(`Testando N° ${n}`)
+    n++
+}*/
+
+//Teste do For
+console.log('Está começando o programa!!')
+for( let n = 15 ; n <= 30 ; n++ ){
+    console.log(`Retornando N°${n}`)
+}
+console.log('Fim da execução!!')
