86b52b1dc3b421426a29717efe07f631cd5abbff
JoaoBerton <141670692+JoaoBerton@users.noreply.github.com>
2026-03-15 13:39:01 -0300

    Correção de algumas falhas no código para rodar corretamente no servidor.

--- aula12/aula16/aplication.js
+++ aula12/aula16/aplication.js
@@ -1,29 +1,32 @@
 function clicar() {
-    //Hora de declarar as variáveis!!
     let iniStr = document.getElementById('inicio').value
     let fnStr = document.getElementById('fim').value
     let pssStr = document.getElementById('passo').value
     let res = document.getElementById('res')
-//Verifica se há algum campo vázio.
-    if(iniStr === "" || fnStr === "" || pssStr === "") {
+
+    if (iniStr === "" || fnStr === "" || pssStr === "") {
         window.alert('[ERRO] nenhum valor listado, por favor tente novamente!')
-        //Faz o teste no início para saber se os dados são válidos.
-    }else {
+    } else {
         let ini = Number(iniStr)
         let fn = Number(fnStr)
         let pss = Number(pssStr)
-        /*Depois se caso der verdadeiro, o valor é convertido para número e testa se o valor é nulo*/
-    if(pss === 0) {
-        res.innerHTML = 'O valor não é válido, por favor tente novamente!'
-        //Caso tudo estiver Ok vem o alerta de confirmação.
-    }else {
-        window.alert('Prossiga com o cálculo!')
-    }
-}
-    for(let calc = ini + pss; calc >= fn; calc += pss) {
-        res.innerHTML = `${calc}`
-        if(calc === fn){
-            res.innerHTML = 'Cálculado!'
+
+        if (pss <= 0) {
+            res.innerHTML = 'O valor do passo não é válido, por favor tente novamente!'
+        } else {
+            res.innerHTML = 'Contando: <br>'
+            if (ini < fn) {
+                // Contagem crescente
+                for (let calc = ini; calc <= fn; calc += pss) {
+                    res.innerHTML += `${calc} 👉 `
+                }
+            } else {
+                // Contagem decrescente
+                for (let calc = ini; calc >= fn; calc -= pss) {
+                    res.innerHTML += `${calc} 👉 `
+                }
+            }
+            res.innerHTML += '🏁'
         }
     }
 }
